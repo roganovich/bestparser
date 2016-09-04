575ced345a8f5cb7476102b5ace9639510d1c72e
roman.roganovich <roman.roganovich@mail.ru>
2016-09-04 10:01:37 +0300

    DAHUA parser

--- index.php
+++ index.php
@@ -12,15 +12,7 @@
                 
              
                 
-                $parser2 = new Builder('Rvi');
-                $bp_rvi = $parser2->getParser('Rvi');
-                $bp_rvi->setParam('base_url','http://rvi-cctv.ru'); /*добавляем параметр базовой страницы*/
-                $bp_rvi->setParam('first_url','http://rvi-cctv.ru/catalog');
-		$bp_rvi->setParam('exportFile','rvi.csv'); /*добавляем параметр файла экспорта*/
-                $bp_rvi->parseDoom();/*запускаем стартовую страницу парсера*/
-                $bp_rvi->getArticles();/*выводим на экран все артикулы*/
-		$bp_rvi->putArticlesToCSV();/*выводим в файл все артикулы*/
-               die(); 
+             
                 $parser3 = new Builder('Dahua');
                 $bp_da = $parser3->getParser('Dahua');
 		$bp_da->setParam('base_url','http://dahua-russia.ru'); /*добавляем параметр базовой страницы*/
@@ -29,8 +21,16 @@
                 $bp_da->parseDoom();/*запускаем стартовую страницу парсера*/
                 $bp_da->getArticles();/*выводим на экран все артикулы*/
 		$bp_da->putArticlesToCSV();/*выводим в файл все артикулы*/
-                
-         
+                  die(); 
+            $parser2 = new Builder('Rvi');
+                $bp_rvi = $parser2->getParser('Rvi');
+                $bp_rvi->setParam('base_url','http://rvi-cctv.ru'); /*добавляем параметр базовой страницы*/
+                $bp_rvi->setParam('first_url','http://rvi-cctv.ru/catalog');
+		$bp_rvi->setParam('exportFile','rvi.csv'); /*добавляем параметр файла экспорта*/
+                $bp_rvi->parseDoom();/*запускаем стартовую страницу парсера*/
+                $bp_rvi->getArticles();/*выводим на экран все артикулы*/
+		$bp_rvi->putArticlesToCSV();/*выводим в файл все артикулы*/
+             
                 
 		 /*Создаем обработчик сайта http://partizan-cctv.ru*/
 		$parser1 = new Builder('Partizan');
